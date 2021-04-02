922e987181a1c8d22b62f999b801f663eb111fe0
xscavx <ur.pekax@gmail.com>
2021-04-02 15:36:40 +0300

    Words-counter implementation

--- task2-words-counter/src/words-counter.cpp
+++ task2-words-counter/src/words-counter.cpp
@@ -2,18 +2,25 @@
 #include <unordered_map>
 
 
-constexpr wchar_t WORDS_DELIMETER{ L' ' };
+// You can complete this list of word's delimeters
+constexpr std::wstring_view WORDS_DELIMETERS{ L" ,:?;()" };
 
 std::wstring_view cut_next_word(std::wstring_view & text) {
-  auto found_pos = text.find(WORDS_DELIMETER);
-  auto word = text.substr(0, found_pos);
-  text.remove_prefix(word.size());
+  auto found_pos = text.find_last_of(WORDS_DELIMETERS);
+  if (found_pos == std::wstring_view::npos) {
+    std::wstring_view word{};
+    text.swap(word);
+    return word;
+  }
+  auto word = text.substr(found_pos);
+  text.remove_suffix(word.size());
   return word;
 }
 
-void count_words_by_length(std::wstring_view text) {
+void count_words_by_length(std::wstring_view text_src) {
   // unordered_map cause of we don't care about order while iterating
   std::unordered_map<std::size_t, std::size_t> length_to_count;
+  std::wstring_view text = text_src;
   while (!text.empty()) {
     auto word = cut_next_word(text);
     length_to_count[word.size()] += 1;
