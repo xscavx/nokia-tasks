25520710cd35aa8e15cd5abd168c85cb1d129af6
xscavx <ur.pekax@gmail.com>
2021-04-05 16:14:58 +0300

    Proper imlementation of tree traverse.

--- task5-tree-paths/include/tree-paths.hpp
+++ task5-tree-paths/include/tree-paths.hpp
@@ -1,1 +1,50 @@
 #pragma once
+
+#include <vector>
+#include <stack>
+
+struct TreeNode {
+  struct TreeNode *leftChild;
+  struct TreeNode *rightChild;
+};
+
+using TreePath = std::stack<TreeNode *>;
+
+auto find_all_deepest_paths(TreeNode *root) {
+  std::vector<TreePath> all_paths;
+  if (!root) {
+    return all_paths;
+  }
+
+  TreePath current_path{{root}};
+  std::size_t max_path_size{0};
+  auto save_path = [&all_paths, &max_path_size](TreePath &path) {
+    if (path.size() < max_path_size) {
+      return;
+    } else if (path.size() > max_path_size) {
+      all_paths.clear();
+      max_path_size = path.size();
+    }
+    all_paths.emplace_back(path);
+  };
+
+  TreeNode *prev_node{nullptr};
+  while (!current_path.empty()) {
+    TreeNode *current_node = current_path.top();
+    TreeNode *left_child = current_node->leftChild;
+    TreeNode *right_child = current_node->rightChild;
+    bool const left_visited = prev_node && (!left_child || prev_node == left_child);
+    bool const right_visited = prev_node && (!right_child || prev_node == right_child);
+
+    if (!left_visited && !right_visited) {
+      current_path.push(left_child);
+    } else if (!right_visited) {
+      current_path.push(right_child);
+    } else {
+      save_path(current_path);
+      current_path.pop();
+    }
+    prev_node = current_node;
+  }
+  return all_paths;
+}
--- tests/task5-tree-paths.cpp
+++ tests/task5-tree-paths.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include "tree-paths.hpp"
-
+#include <memory>
 
 class TreePaths : public testing::Test {
   public:
@@ -19,5 +19,30 @@ class TreePaths : public testing::Test {
 
 GTEST_TEST(TreePaths, Simpliest)
 {
+  std::vector<std::unique_ptr<TreeNode>> allocated_nodes;
+  auto alloc_node = [&allocated_nodes]() -> TreeNode * {
+    return allocated_nodes.emplace_back(std::make_unique<TreeNode>()).get();
+  };
+
+  TreeNode * root = alloc_node();
+  root->leftChild = alloc_node();
+  root->rightChild = alloc_node();
+  TreeNode * cur = root->leftChild;
+  cur->leftChild = alloc_node();
+  cur->rightChild = alloc_node();
+  cur = root->rightChild;
+  cur->leftChild = alloc_node();
+  cur->rightChild = alloc_node();
+  cur = cur->rightChild;
+  cur->leftChild = alloc_node();
+  auto paths = find_all_deepest_paths(root);
+
+  cur->rightChild = cur->leftChild;
+  cur->leftChild = nullptr;
+  paths = find_all_deepest_paths(root);
+
+  cur->leftChild = alloc_node();
+  paths = find_all_deepest_paths(root);
+
   ASSERT_EQ(0, 0);
 }
