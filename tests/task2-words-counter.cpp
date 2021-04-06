ea4e05f43cf96950e71df8e8ae8f6c9ae58a8f3e
xscavx <ur.pekax@gmail.com>
2021-04-06 13:32:42 +0300

    Proper test for words counter

--- tests/task2-words-counter.cpp
+++ tests/task2-words-counter.cpp
@@ -20,10 +20,13 @@ class TestWordsCounter : public testing::Test {
 TEST(TestWordsCounter, Simpliest) {
   constexpr std::size_t MAX_WORD_LEN{ 10 };
   auto count_array = count_words_by_length<MAX_WORD_LEN>(L"FIVE words in this text");
+  EXPECT_EQ(count_array[4], 3);
+
   count_array = count_words_by_length<MAX_WORD_LEN>(L"1 2 3  4  1a 2a    3a");
-  EXPECT_EQ(0, 0);
+  EXPECT_EQ(count_array[1], 4);
 }
 
 TEST(TestWordsCounter, LongWords) {
+  // TODO: generate text, count words
   EXPECT_EQ(0, 0);
 }
